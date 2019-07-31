2d92e6547cc7e9ab27073bd0e1699670735ca365
Moh Eric <maseric7@gmail.com>
2019-07-31 07:08:13 +0700

    Add initialCharacters method

--- src/com/rickseven/java/startext/StarText.java
+++ src/com/rickseven/java/startext/StarText.java
@@ -1,21 +1,56 @@
 package com.rickseven.java.startext;
 
 import com.rickseven.java.startext.characters.*;
+import com.rickseven.java.startext.characters.Character;
+
+import java.util.HashMap;
 
 class StarText {
 
     private String text;
     private ICharacterOption option;
+    private HashMap<java.lang.Character, Character> characters = new HashMap<>();
 
     StarText(String text, ICharacterOption option){
-        this.text = text;
+        this.text = text.toUpperCase();
         this.option = option;
     }
 
+    private void initialCharacters(){
+        characters.put('A', A.getInstance(this.option));
+        characters.put('B', B.getInstance(this.option));
+        characters.put('C', C.getInstance(this.option));
+        characters.put('D', D.getInstance(this.option));
+        characters.put('E', E.getInstance(this.option));
+        characters.put('F', F.getInstance(this.option));
+        characters.put('G', G.getInstance(this.option));
+        characters.put('H', H.getInstance(this.option));
+        characters.put('I', I.getInstance(this.option));
+        characters.put('J', J.getInstance(this.option));
+        characters.put('K', K.getInstance(this.option));
+        characters.put('L', L.getInstance(this.option));
+        characters.put('M', M.getInstance(this.option));
+        characters.put('N', N.getInstance(this.option));
+        characters.put('O', O.getInstance(this.option));
+        characters.put('P', P.getInstance(this.option));
+        characters.put('Q', Q.getInstance(this.option));
+        characters.put('R', R.getInstance(this.option));
+        characters.put('S', S.getInstance(this.option));
+        characters.put('T', T.getInstance(this.option));
+        characters.put('U', U.getInstance(this.option));
+        characters.put('V', V.getInstance(this.option));
+        characters.put('W', W.getInstance(this.option));
+        characters.put('X', X.getInstance(this.option));
+        characters.put('Y', Y.getInstance(this.option));
+        characters.put('Z', Z.getInstance(this.option));
+    }
+
     void print(){
-        char[] charArr = new char[text.length()];
-        for(int i = 0; i < text.length(); i++){
-            charArr[i] = text.charAt(i);
+        initialCharacters();
+
+        Character[] charArr = new Character[this.text.length()];
+        for(int i = 0; i < this.text.length(); i++){
+            charArr[i] = characters.getOrDefault(this.text.charAt(i), null);
         }
 
         int n = this.option.getSize();
@@ -30,34 +65,10 @@ class StarText {
                     d++;
                 }
 
-                switch(charArr[d]){
-                    case 'A' : A.getInstance(option).printChar(i, c); break;
-                    case 'B' : B.getInstance(option).printChar(i, c); break;
-                    case 'C' : C.getInstance(option).printChar(i, c); break;
-                    case 'D' : D.getInstance(option).printChar(i, c); break;
-                    case 'E' : E.getInstance(option).printChar(i, c); break;
-                    case 'F' : F.getInstance(option).printChar(i, c); break;
-                    case 'G' : G.getInstance(option).printChar(i, c); break;
-                    case 'H' : H.getInstance(option).printChar(i, c); break;
-                    case 'I' : I.getInstance(option).printChar(i, c); break;
-                    case 'J' : J.getInstance(option).printChar(i, c); break;
-                    case 'K' : K.getInstance(option).printChar(i, c); break;
-                    case 'L' : L.getInstance(option).printChar(i, c); break;
-                    case 'M' : M.getInstance(option).printChar(i, c); break;
-                    case 'N' : N.getInstance(option).printChar(i, c); break;
-                    case 'O' : O.getInstance(option).printChar(i, c); break;
-                    case 'P' : P.getInstance(option).printChar(i, c); break;
-                    case 'Q' : Q.getInstance(option).printChar(i, c); break;
-                    case 'R' : R.getInstance(option).printChar(i, c); break;
-                    case 'S' : S.getInstance(option).printChar(i, c); break;
-                    case 'T' : T.getInstance(option).printChar(i, c); break;
-                    case 'U' : U.getInstance(option).printChar(i, c); break;
-                    case 'V' : V.getInstance(option).printChar(i, c); break;
-                    case 'W' : W.getInstance(option).printChar(i, c); break;
-                    case 'X' : X.getInstance(option).printChar(i, c); break;
-                    case 'Y' : Y.getInstance(option).printChar(i, c); break;
-                    case 'Z' : Z.getInstance(option).printChar(i, c); break;
-                    default: System.out.print(" "); break;
+                if(charArr[d] != null){
+                    charArr[d].printChar(i, c);
+                }else{
+                    System.out.print(" ");
                 }
 
                 c++;
